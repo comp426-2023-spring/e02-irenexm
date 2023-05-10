30be50d26eccb517c322339c4f4541b1222324b0
Irene <irenexm@email.unc.edu>
2023-05-09 23:43:00 -0400

    script fixes and added screenshot

--- public/script.js
+++ public/script.js
@@ -2,28 +2,85 @@
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
 
-const rock = document.querySelector('#rock');
-const paper = document.querySelector('#paper');
-const scissors = document.querySelector('#scissors');
-const lizard = document.querySelector('#lizard');
-const spock = document.querySelector('#spock');
-
-const resultText = document.querySelector('.game-output')
-
-function displayOptions() {
-
+function showOptions() {
+    shots = document.querySelector("#shot-options");
+    if (document.getElementById("opponent").checked) {
+        shots.className = "active";
+    } else {
+        shots.className = "inactive";
+    }
 }
-    
-function playGame() {
-    const url = "/app/rpsls/play/" + shot;
-    return response = fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            return data;
+
+function showRpsls() {
+    let rpsShots = document.getElementsByName("rps-shot");
+    rpsShots.forEach(rpsShot => {
+        rpsShot.className = "active";
+    })
+
+    let rpslsShots = document.getElementsByName("rpsls-shot");
+    if (document.getElementById("rpsls").checked) {
+        rpslsShots.forEach(rpslsShot => {
+            rpslsShot.className = "active";
         })
-        .catch(error => console.error(error));
+    } else {
+        rpslsShots.forEach(rpslsShot => {
+            rpslsShot.className = "inactive";
+        })
+    }
+}
+
+    
+
+    
+async function playGame() {
+    const opponent = document.getElementById("opponent").checked;
+    const mode = document.getElementById("rps").checked ? "rps" : "rpsls";
+    
+    document.querySelector("#mode-options").className = "inactive";
+    document.querySelector("#shot-options").className = "inactive";
+    document.querySelector("#game-output").className = "active";
+    document.querySelector("#play-button").className = "inactive";
+
+    let shot = "";
+    if (opponent) {
+        shot = document.querySelector('input[type="radio"][name*="-shot"]:checked').value;
+    }
+
+    const url = `/app/${mode}/play/${shot}`;;
+    console.log(url);
+
+    try {
+        const response = await fetch(url);
+        const result = await response.json();
+        console.log(result)
+
+        const outputText = document.querySelector("#game-output")
+        if (opponent) {
+            outputText.innerHTML = `<p>you picked: ${result.player}</p>
+            <p>your opponent picked: ${result.opponent}</p>
+            <p>result: ${result.result}</p>`
+        } else {
+            outputText.innerHTML = `<p>you got ${result.player}!</p>`
+        }
+    } catch (error) {
+        console.error("Error fetching game result:", error);
+        alert("Error fetching game result. Please try again later.");
+    }
+
 }
 
 function resetGame() {
+    document.querySelector("#mode-options").className = "active";
+    document.querySelector("#shot-options").className = "inactive";
+    document.querySelector("#game-output").className = "inactive";
+    document.querySelector("#play-button").className = "active";
 
+    document.getElementById("rps").checked = true;
+    document.getElementById("rpsls").checked = false;
+    document.getElementById("opponent").checked = false;
+    document.getElementById("rock").checked = false;
+    document.getElementById("paper").checked = false;
+    document.getElementById("scissors").checked = false;
+    document.getElementById("lizard").checked = false;
+    document.getElementById("spock").checked = false;
 }
--- server.js
+++ server.js
@@ -71,6 +71,67 @@ app.use(morgan(':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:htt
 // Serve static files
 const staticpath = args.stat || args.s || process.env.STATICPATH || path.join(__dirname, 'public')
 app.use('/', express.static(staticpath))
+
+
+/* a04 */
+import { rps, rpsls } from "./lib/rpsls.js"
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/app', (req, res) => {
+    res.status(200).send('200 OK').end();
+});
+
+// random rps shot
+app.get('/app/rps', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
+});
+
+// random rpsls shot
+app.get('/app/rpsls', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
+});
+
+// play rps
+app.get('/app/rps/play', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.query.shot))).end();
+});
+
+// play rpsls
+app.get('/app/rpsls/play', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.query.shot))).end();
+});
+
+// play rps
+app.post('/app/rps/play', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
+});
+
+// play rpsls
+app.post('/app/rpsls/play', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
+});
+
+// play rps with shot
+// /app/rpsls/play/scissors leads to shot = scissors
+app.get('/app/rps/play/:shot', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.params.shot))).end();
+});
+
+// play rpsls with shot
+app.get('/app/rpsls/play/:shot', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.params.shot))).end();
+});
+
+// catch-all endpoint, throw 404 error for other requests
+app.get('*', (req, res) => {
+    res.status(404).send('404 NOT FOUND').end();
+});
+
+
+/* a04 end */
+
+
 // Create app listener
 const server = app.listen(port)
 // Create a log entry on start
